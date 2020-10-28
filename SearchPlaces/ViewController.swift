1bf3badaab39777d4a5eb5c020ab67f3c008da0c
John Joseph Santos <john_joseph.santos@bell.ca>
2020-10-28 14:18:14 +0800

    Initial Commit

---
+++ SearchPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchPlaces
+//
+//  Created by John Joseph M. Santos on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
