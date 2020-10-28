7097cecaf36c7958c3874719ead0d8dfe64c07ec
John Joseph Santos <john_joseph.santos@bell.ca>
2020-10-28 14:26:26 +0800

    Modify Main.storyboard, HomeViewController and HomeViewPresenter files

---
+++ SearchPlaces/Home/HomeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewController.swift
+//  SearchPlaces
+//
+//  Created by John Joseph M. Santos on 10/28/20.
+//
+
+import Foundation
---
+++ SearchPlaces/Home/HomeViewPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewPresenter.swift
+//  SearchPlaces
+//
+//  Created by John Joseph M. Santos on 10/28/20.
+//
+
+import Foundation
